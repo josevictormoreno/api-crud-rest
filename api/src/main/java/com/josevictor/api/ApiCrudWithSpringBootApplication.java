1b62c472ebc58dd4be8eb92c4e4b9ead8ee59921
josevictormoreno <josevictor.admoreno@gmail.com>
2022-04-26 11:44:37 -0300

    Commit inicial - Adicionando Spring-Boot

---
+++ api/src/main/java/com/josevictor/api/ApiCrudWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.josevictor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/josevictor/api/ApiCrudWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josevictor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
